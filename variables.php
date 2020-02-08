9a789669c1cea299d0731f1738ad29c9d3128328
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-02-07 20:51:35 -0700

    Variables

---
+++ variables.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+$first_name = "Kenneth";
+$last_name = "Caple";
+
+$street = "1612 East Ocotillo";
+$city = "Phoenix";
+$state = "AZ";
+$zip = 85281;
+
+$phone = "480-420-9602";
+
+// Print the address:
+print "<p>$first_name $last_name
+<br>$street 
+<br>$city, $state $zip
+<br>$phone</p>";
+
+?>
+</body>
+</html>
