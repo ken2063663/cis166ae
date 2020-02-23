debe296ce977fd115927f95e5af0d694f7294498
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-02-23 16:22:10 -0700

    Creating Random Numbers

---
+++ randon_number.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Random Numbers</title>
+</head>
+<body>
+<?php 
+echo "Your random number is: ";
+echo mt_rand(1, 100);
+
+?>
+</body>
+</html>
