7cd1232223733328d1d5731bb5cfa6e250553d36
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-03-01 19:22:01 -0700

    Taking HTML Out of PHP

--- handle_post.php
+++ handle_post.php
@@ -5,7 +5,7 @@
 	<title>Forum Posting</title>
 </head>
 <body>
-<?php // Script 5.2 - handle_post.php
+<?php // Script 5.4 - handle_post.php #3
 /* This script receives five values from posting.html:
 first_name, last_name, email, posting, submit */
 
@@ -14,14 +14,20 @@ first_name, last_name, email, posting, submit */
 // Get the values from the $_POST array:
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
-$posting = nl2br($_POST['posting'], false);
+$posting = nl2br($_POST['posting']);
 
 // Create a full name variable:
 $name = $first_name . ' ' . $last_name;
 
+// Adjust for HTML tags:
+$html_post = htmlentities($_POST['posting']);
+$strip_post = strip_tags($_POST['posting']);
+
 // Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>$posting</p></div>";
+<p>Original: $posting</p>
+<p>Entity: $html_post</p>
+<p>Stripped: $strip_post</p></div>";
 
 ?>
 </body>
