b6fa2ddbe4ca61b623d1b113f25545d3130d6ab7
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-03-01 19:27:04 -0700

    Encoding and Decoding Strings

--- handle_post.php
+++ handle_post.php
@@ -5,7 +5,7 @@
 	<title>Forum Posting</title>
 </head>
 <body>
-<?php // Script 5.4 - handle_post.php #3
+<?php // Script 5.5 - handle_post.php #4
 /* This script receives five values from posting.html:
 first_name, last_name, email, posting, submit */
 
@@ -19,15 +19,14 @@ $posting = nl2br($_POST['posting']);
 // Create a full name variable:
 $name = $first_name . ' ' . $last_name;
 
-// Adjust for HTML tags:
-$html_post = htmlentities($_POST['posting']);
-$strip_post = strip_tags($_POST['posting']);
-
 // Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>Original: $posting</p>
-<p>Entity: $html_post</p>
-<p>Stripped: $strip_post</p></div>";
+<p>$posting</p></div>";
+
+// Make a link to another page:
+$name = urlencode($name);
+$email = urlencode($_POST['email']);
+print "<p>Click <a href=\"thanks.php?name=$name&email=$email\">here</a> to continue.</p>";
 
 ?>
 </body>
