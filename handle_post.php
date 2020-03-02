8cac803f758a2740f74ebf813329017dc02092e8
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-03-01 19:41:35 -0700

    Update the Book's Program for Substrings

--- handle_post.php
+++ handle_post.php
@@ -5,7 +5,7 @@
 	<title>Forum Posting</title>
 </head>
 <body>
-<?php // Script 5.5 - handle_post.php #4
+<?php // Script 5.6 - handle_post.php #5
 /* This script receives five values from posting.html:
 first_name, last_name, email, posting, submit */
 
@@ -19,14 +19,16 @@ $posting = nl2br($_POST['posting']);
 // Create a full name variable:
 $name = $first_name . ' ' . $last_name;
 
+// Get a word count:
+$words = str_word_count($posting);
+
+// Get a snippet of the posting:
+$posting = substr($posting, 0, 50);
+
 // Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>$posting</p></div>";
-
-// Make a link to another page:
-$name = urlencode($name);
-$email = urlencode($_POST['email']);
-print "<p>Click <a href=\"thanks.php?name=$name&email=$email\">here</a> to continue.</p>";
+<p>$posting...</p>
+<p>($words words)</p></div>";
 
 ?>
 </body>
