74c83e1bc5c3e6a4f133863d4f6394b8c23f059f
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-03-01 19:44:38 -0700

    Update the Books's Program Pages 118-119

--- handle_post.php
+++ handle_post.php
@@ -5,16 +5,17 @@
 	<title>Forum Posting</title>
 </head>
 <body>
-<?php // Script 5.6 - handle_post.php #5
+<?php // Script 5.7 - handle_post.php #6
 /* This script receives five values from posting.html:
 first_name, last_name, email, posting, submit */
 
 // Address error management, if you want.
 
-// Get the values from the $_POST array:
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$posting = nl2br($_POST['posting']);
+// Get the values from the $_POST array.
+// Strip away extra spaces using trim():
+$first_name = trim($_POST['first_name']);
+$last_name = trim($_POST['last_name']);
+$posting = trim($_POST['posting']);
 
 // Create a full name variable:
 $name = $first_name . ' ' . $last_name;
@@ -22,12 +23,12 @@ $name = $first_name . ' ' . $last_name;
 // Get a word count:
 $words = str_word_count($posting);
 
-// Get a snippet of the posting:
-$posting = substr($posting, 0, 50);
+// Take out the bad words:
+$posting = str_ireplace('badword', 'XXXXX', $posting);
 
 // Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>$posting...</p>
+<p>$posting</p>
 <p>($words words)</p></div>";
 
 ?>
