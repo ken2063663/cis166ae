cecb34eab9f6aa9ba7031c5835c82d2af4ae4ce6
ken2063663 <60527674+ken2063663@users.noreply.github.com>
2020-03-08 22:17:14 -0700

    Continuing the Validation Functions of a Web Registration Page

---
+++ assignment-parta.php
@@ -0,0 +1,14 @@
+
+isset() can check to confirm that a variable has a value, and is also used to validate form elements.
+ 
+<?php echo isset($myvar); ?>
+
+empty() checks to see if the variable exists, but IS empty.
+
+<?php echo empty($myvar); ?>
+
+is_mumeric() checks to see if a variable has a valid numerical value.
+
+<?php echo is_numeric($myvar); ?>
+ 
+ 
